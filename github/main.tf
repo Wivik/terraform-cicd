fef075f82b076a0720753c696da87cfcaa072def
Wivik <48727868+Wivik@users.noreply.github.com>
2022-12-18 22:01:13 +0100

    removed useless files

--- github/main.tf
+++ github/main.tf
@@ -3,3 +3,52 @@ provider "github" {
     token = var.GITHUB_TOKEN
     owner = var.organization
 }
+
+## create repositories declared in vars
+
+## Create the repository
+
+resource "github_repository" "repository" {
+    for_each = var.repositories
+    name = each.value
+    visibility = "private"
+    vulnerability_alerts = true
+
+    template {
+        owner = var.organization
+        repository = var.repository_template
+    }
+}
+
+## Netsted loop for repositories, teams and roles
+
+locals {
+    repositories = var.repositories
+    teams_roles = var.teams_roles
+    repository_team = flatten([
+        for repository in local.repositories : [
+            for role in local.teams_roles : {
+                repository = repository
+                role = role
+            }
+        ]
+    ])
+}
+
+## create its teams
+
+resource "github_team" "teams" {
+    for_each = { for entry in local.repository_team: "${entry.repository}-team-${entry.role}" => entry }
+    name = "${each.value.repository}-team-${each.value.role}"
+    # create_default_maintainer = true
+}
+
+## Assign teams
+
+resource "github_team_repository" "dept-1-app-1-team-repo" {
+    for_each = { for entry in local.repository_team: "${entry.repository}-team-${entry.role}" => entry }
+    team_id = "${each.value.repository}-${each.value.role}"
+    permission = each.value.role
+    repository = each.value.repository
+}
+
--- github/vars.tf
+++ github/vars.tf
@@ -1,16 +1,32 @@
+## The GITHUB_TOKEN PAT that'ill execute the actions
+## It's an environment variable named TF_VARS_GITHUB_TOKEN that provide it
 variable "GITHUB_TOKEN" {
     type = string
 }
 
+## The target organization
 variable "organization" {
     default = "poochi-corp"
 }
 
+## The default repository template
 variable "repository_template" {
     default = "std-template-repository"
 }
 
+## The teams roles to create for each repository
 variable "teams_roles" {
     type = set(string)
-    default = ["read", "write", "admin"]
+    default = ["push", "admin"]
+}
+
+## The repositories list
+variable "repositories" {
+    type=set(string)
+    default = [
+        "dept-1-app-1",
+        # "dept-2-app-1",
+        # "dept-3-app-1",
+        # "dept-4-app-1",
+    ]
 }
